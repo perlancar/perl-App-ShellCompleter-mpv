69a1d960491f7c3ef89ed302582b215d4ed25092
perlancar (@pc-office) <perlancar@gmail.com>
2015-08-06 14:11:31 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/mpv.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::mpv;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for mpv
+
+=head1 SYNOPSIS
+
+See L<_mpv> included in this distribution.
+
+
+=head1 SEE ALSO
+
+mpv, L<http://mpv.io/>
